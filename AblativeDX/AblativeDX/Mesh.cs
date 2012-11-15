0507fd174eba5815d5e301b828dd0348b0c44f87
Dave <dkushner2@mail.csuchico.edu>
2012-11-14 23:43:46 -0800

    First commit of solution.
    Basic model loading functionality is theoretically working. Build
    currently fails on a bad call across the managed-unmanaged domain to
    Assimp32.dll.

---
+++ AblativeDX/AblativeDX/Mesh.cs
@@ -0,0 +1,47 @@
+﻿using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
+using System.Linq;
+
+namespace AblativeDX
+{
+    public sealed class Mesh : System.IDisposable
+    {
+        public string Name;
+
+        public Buffer<VertexPositionColor> VertexBuffer;
+        public VertexInputLayout InputLayout;
+
+        private GraphicsDevice graphicsDevice;
+        public BasicEffect Effect;
+
+        internal Mesh(GraphicsDevice graphicsDevice, Vector3[] vertices, Color[] colors)
+        {
+            this.graphicsDevice = graphicsDevice;
+            this.Effect = new BasicEffect(graphicsDevice);
+
+            this.VertexBuffer = Buffer.Vertex.New(graphicsDevice,
+                (from vertex in vertices
+                from color in colors
+                select new VertexPositionColor()
+                {
+                    Position = vertex,
+                    Color = color
+                }).ToArray());
+            this.InputLayout = VertexInputLayout.FromBuffer(0, VertexBuffer);
+        }
+        internal void Draw()
+        {
+            this.graphicsDevice.SetVertexBuffer(VertexBuffer);
+            this.graphicsDevice.SetVertexInputLayout(InputLayout);
+
+            Effect.CurrentTechnique.Passes[0].Apply();
+            graphicsDevice.Draw(PrimitiveType.TriangleList, VertexBuffer.ElementCount);
+        }
+        public void Dispose()
+        {
+            Effect.Dispose();
+        }
+    }
+}
+
---
+++ AblativeDX/AblativeDX/SceneRenderer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+using Assimp;
+using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Content;
+
+
+namespace AblativeDX
+{
+    using SharpDX.Toolkit.Graphics;
+
+    public class SceneRenderer : Game
+    {
+        private GraphicsDeviceManager graphicsDeviceManager;
+        private BasicEffect basicEffect;
+        private Buffer<VertexPositionColor> vertices;
+        private VertexInputLayout inputLayout;
+
+        public SceneRenderer()
+        {
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Assets";
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "HelloWorld!";
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            Model thing = Content.Load<Model>("Mousetest.fbx");
+
+            base.LoadContent();
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            var time = (float)gameTime.TotalGameTime.TotalSeconds;
+            basicEffect.World = Matrix.RotationX(time) * Matrix.RotationY(time * 2.0f) * Matrix.RotationZ(time * 0.7f);
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(GraphicsDevice.BackBuffer, Color.CornflowerBlue);
+
+            GraphicsDevice.SetVertexBuffer(vertices);
+            GraphicsDevice.SetVertexInputLayout(inputLayout);
+
+            basicEffect.CurrentTechnique.Passes[0].Apply();
+            GraphicsDevice.Draw(PrimitiveType.TriangleList, vertices.ElementCount);
+
+            base.Draw(gameTime);
+        }
+    }
+}
